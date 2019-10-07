31b6629f3758a1995a7c2ba42ff955b2ba06a762
Leonard A. Harris <lh64@cornell.edu>
2019-10-07 08:12:58 -0500

    Made separate Parameters and Expressions for the effects of Non-NE cells on the division and death rates of each NE variant.

--- four_state_sclc.py
+++ four_state_sclc.py
@@ -4,7 +4,7 @@ from pysb.simulator import BngSimulator
 import numpy as np
 import matplotlib.pyplot as plt
 
-def k_fate(ename, k_fate_0, KD_Kx_fate, k_fate_x, effector_cell_obs):
+def k_fate(ename, k_fate_0, k_fate_x, KD_Kx_fate, effector_cell_obs):
     return Expression(ename, (k_fate_0*KD_Kx_fate + k_fate_x*effector_cell_obs) / (KD_Kx_fate + effector_cell_obs))
 
 Model()
@@ -23,37 +23,56 @@ Observable('NEv2_obs', NEv2())
 Observable('NonNE_obs', NonNE())
 Observable('NE_all', NE()+NEv1()+NEv2())
 
+# Parameter('k_ne_div', 1)
 Parameter('k_NE_div_0', 1) # TPCs divide approximately once per day in culture
 Parameter('k_NE_div_x', 2)
 Parameter('KD_Kx_NE_div', 1000)
-k_fate('k_NE_div', k_NE_div_0, KD_Kx_NE_div, k_NE_div_x, NonNE_obs)
+k_fate('k_NE_div', k_NE_div_0, k_NE_div_x, KD_Kx_NE_div, NonNE_obs)
+Rule('NE_div', NE() >> NE() + NE(), k_NE_div)
+
+# Parameter('k_ne_die', 0.9)
 Parameter('k_NE_die_0', 0.9)
 Parameter('k_NE_die_x', 0.1)
 Parameter('KD_Kx_NE_die', 1000)
-k_fate('k_NE_die', k_NE_die_0, KD_Kx_NE_die, k_NE_die_x, NonNE_obs)
-
-# Parameter('k_ne_div', 1)
-# Parameter('k_ne_die', 0.9)
-Rule('NE_div', NE() >> NE() + NE(), k_NE_div)
+k_fate('k_NE_die', k_NE_die_0, k_NE_die_x, KD_Kx_NE_die, NonNE_obs)
 Rule('NE_die', NE() >> None, k_NE_die)
 
 # Parameter('k_nev1_div', 1)
+Parameter('k_NEv1_div_0', 1) # TPCs divide approximately once per day in culture
+Parameter('k_NEv1_div_x', 2)
+Parameter('KD_Kx_NEv1_div', 1000)
+k_fate('k_NEv1_div', k_NEv1_div_0, k_NEv1_div_x, KD_Kx_NEv1_div, NonNE_obs)
+Rule('NEv1_div', NEv1() >> NEv1() + NEv1(), k_NEv1_div)
+
 # Parameter('k_nev1_die', 0.9)
-Rule('NEv1_div', NEv1() >> NEv1() + NEv1(), k_NE_div)
-Rule('NEv1_die', NEv1() >> None, k_NE_die)
+Parameter('k_NEv1_die_0', 0.9)
+Parameter('k_NEv1_die_x', 0.1)
+Parameter('KD_Kx_NEv1_die', 1000)
+k_fate('k_NEv1_die', k_NEv1_die_0, k_NEv1_die_x, KD_Kx_NEv1_die, NonNE_obs)
+Rule('NEv1_die', NEv1() >> None, k_NEv1_die)
 
 # Parameter('k_nev2_div', 1)
+Parameter('k_NEv2_div_0', 1) # TPCs divide approximately once per day in culture
+Parameter('k_NEv2_div_x', 2)
+Parameter('KD_Kx_NEv2_div', 1000)
+k_fate('k_NEv2_div', k_NEv2_div_0, k_NEv2_div_x, KD_Kx_NEv2_div, NonNE_obs)
+Rule('NEv2_div', NEv2() >> NEv2() + NEv2(), k_NEv2_div)
+
 # Parameter('k_nev2_die', 0.9)
-Rule('NEv2_div', NEv2() >> NEv2() + NEv2(), k_NE_div)
-Rule('NEv2_die', NEv2() >> None, k_NE_die)
+Parameter('k_NEv2_die_0', 0.9)
+Parameter('k_NEv2_die_x', 0.1)
+Parameter('KD_Kx_NEv2_die', 1000)
+k_fate('k_NEv2_die', k_NEv2_die_0, k_NEv2_die_x, KD_Kx_NEv2_die, NonNE_obs)
+Rule('NEv2_die', NEv2() >> None, k_NEv2_die)
 
 # Parameter('k_nonNe_div', 0.9)
 Parameter('k_nonNE_div_0', 1.1)
 Parameter('k_nonNE_div_x', 0.9)
 Parameter('KD_Kx_nonNE_div', 1000)
-k_fate('k_nonNE_div', k_nonNE_div_0, KD_Kx_nonNE_div, k_nonNE_div_x, NE_all)
-Parameter('k_nonNe_die', 0.1)
+k_fate('k_nonNE_div', k_nonNE_div_0, k_nonNE_div_x, KD_Kx_nonNE_div, NE_all)
 Rule('NonNE_div', NonNE() >> NonNE() + NonNE(), k_nonNE_div)
+
+Parameter('k_nonNe_die', 0.1)
 Rule('NonNE_die', NonNE() >> None, k_nonNe_die)
 
 Parameter('kf_diff_ne_nev1', 0.1)
